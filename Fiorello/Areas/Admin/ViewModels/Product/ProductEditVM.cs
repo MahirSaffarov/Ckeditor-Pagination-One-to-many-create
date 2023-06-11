40b451a7e26866d171593470573bc99434779168
Mahir <mahirgs@code.edu.az>
2023-06-11 08:41:37 +0400

    Ckeditor, Pagination, One-to-many create

---
+++ Fiorello/Areas/Admin/ViewModels/Product/ProductEditVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace Fiorello.Areas.Admin.ViewModels.Product
+{
+    public class ProductEditVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public int DiscountId { get; set; }
+        public ICollection<string> Images { get; set; }
+        public List<IFormFile> NewImage { get; set; }
+        public ICollection<bool> IsMain { get; set; }
+    }
+}
---
+++ Fiorello/wwwroot/admin/js/product.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $(document).on("click", "#delete", function (e) {
+        e.preventDefault();
+        console.log("skfbnskfj")
+        let productId = $(this).attr("data-id");
+
+
+        let data = { id: productId };
+
+        $.ajax({
+            url: '/Product/Edit',
+            type: 'POST',
+            data: data, 
+            success: function (response) {
+            }
+        });
+
+    })
+})
